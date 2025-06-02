bd13a4066ea265f7f66588ace5f50f53416cb3cb
MessiahSastry <messiahsastry20@gmail.com>
2025-06-02 15:22:07 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -104,7 +104,7 @@ settingsPopup.addEventListener("click", (e) => {
 
 // HOOK UP DASHBOARD BUTTONS
 document.getElementById("gen-ai-btn").onclick = function() {
-  window.open("https://22f773db-8a2d-4142-9ba1-88c12864daa0-00-1civ713u5lhhg.pike.replit.dev/ai-question-generator.html", "_blank");
+  window.open("ai-question-generator.html", "_blank");
 };
 document.getElementById("manual-add-btn").onclick = function() {
   alert("Manual Question Entry coming soon!");
