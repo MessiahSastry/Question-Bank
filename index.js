6ed8030042c6557662d0c9d61bccd3e100672ee6
MessiahSastry <messiahsastry20@gmail.com>
2025-06-03 08:39:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const db = firebase.firestore();
 
 
 // ====== SECTION NAVIGATION ======
-function showSection(id) {
+function showSection(id, pushState = true) {
   document.querySelectorAll('.app-section').forEach(sec => sec.style.display = 'none');
   document.getElementById(id).style.display = 'block';
+  if (pushState) {
+    history.pushState({ section: id }, '', '#' + id);
+  }
 }
 
 // ====== SPLASH AND LOGIN ======
