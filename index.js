3d194f320933495294c54df2b6d495e4c7f21727
MessiahSastry <messiahsastry20@gmail.com>
2025-06-04 00:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ function showLoginUI() {
       <div class="login-title">ST. PATRICK'S SCHOOL</div>
       <div class="login-subtitle">IIT & NEET FOUNDATION</div>
       </div>
-      </header>
-      <input type="email" id="email" placeholder="Email" autocomplete="username" />
+     <input type="email" id="email" placeholder="Email" autocomplete="username" />
       <input type="password" id="password" placeholder="Password" autocomplete="current-password" />
       <div class="forgot-row">
         <button type="button" onclick="forgotPassword()">Forgot Password?</button>
