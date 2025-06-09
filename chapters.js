6d543c68df53c01fdde794ad17c1f90bef9b32b1
MessiahSastry <messiahsastry20@gmail.com>
2025-06-09 11:24:18 +0530

    Add files via upload

---
+++ chapters.js
@@ -0,0 +1,691 @@
+window.textbooks = {
+  "6th Class": {
+    "Telugu": [
+      "మన ఊరు – మనం",
+      "ఊరి దీవెన",
+      "పాట",
+      "సరదా సమయం",
+      "చిన్నారి భూమిక",
+      "మచ్చలు",
+      "మనుషులే దేవుళ్లు",
+      "శ్రీకృష్ణ లీలలు"
+    ],
+    "Hindi": [
+      "वह चिड़िया जो",
+      "बचपन",
+      "नादान दोस्त",
+      "चाँद से थोड़ी सी गप्पें",
+      "अक्षरों का महत्व",
+      "पार नज़र के",
+      "साथी हाथ बढ़ाना",
+      "ऐसे-ऐसे",
+      "टिकट एल्बम",
+      "झाँसी की रानी"
+    ],
+    "English 1": [
+      "Who Did Patrick’s Homework?",
+      "I Want Peace",
+      "I Wonder",
+      "The Selfish Giant",
+      "A Lesson for All",
+      "The Bond of Love",
+      "The Naughty Boy",
+      "The Monkey and the Crocodile",
+      "The School for Sympathy",
+      "What Can a Dollar and Eleven Cents Do?"
+    ],
+    "English 2": [],
+    "Math 1": [
+      "Knowing Our Numbers",
+      "Whole Numbers",
+      "Playing with Numbers",
+      "Basic Geometrical Ideas",
+      "Understanding Elementary Shapes",
+      "Integers"
+    ],
+    "Math 1 IIT": [
+      "Tables, Squares and Cubes",
+      "Knowing Our Numbers",
+      "Roman Numerals",
+      "Whole Numbers",
+      "Playing With Numbers",
+      "LCM and HCF",
+      "Basic Geometrical Ideas",
+      "Measures of Lines and Angles",
+      "Integers",
+      "Fractions and Decimals",
+      "Rational Numbers"
+    ],
+    "Math 2": [
+      "Fractions",
+      "Decimals",
+      "Data Handling",
+      "Mensuration",
+      "Algebra",
+      "Ratio and Proportion",
+      "Symmetry",
+      "Practical Geometry"
+    ],
+    "Math IIT": [],
+    "Physics": [
+      "Our Food",
+      "Materials and Their Properties",
+      "Separation of Substances",
+      "Changes Around Us",
+      "Water",
+      "Magnets",
+      "Motion and Measurement"
+    ],
+    "Physics IIT & NEET": [
+      "Units and Measurements",
+      "Kinematics",
+      "Newton's Laws of Motion",
+      "Work-Energy and Power",
+      "Heat",
+      "Light",
+      "Electrostatics",
+      "Magnetism"
+    ],
+    "Chemistry": [
+      "Materials and Their Properties",
+      "Separation of Substances",
+      "Air Around Us"
+    ],
+    "Chemistry IIT & NEET": [
+      "Nature of Chemistry",
+      "Language of Chemistry",
+      "Basic Concept Of Chemistry",
+      "Atomic Structure",
+      "Chemical Bonding",
+      "Acids, Bases and Salts",
+      "Allotropy of Carbon",
+      "Transformation of Substances",
+      "Water",
+      "Air",
+      "Apparatus in Chemical Laboratory"
+    ],
+    "Biology": [
+      "The Living World",
+      "Plant World",
+      "Animal World",
+      "Our Environment"
+    ],
+    "Biology NEET": [
+      "Nutrition in Plants",
+      "Nutrition in Animals",
+      "Weather, Climate & Adaptation",
+      "Soil",
+      "Respiration in Organisms",
+      "Transportation in Animals and Plants",
+      "Reproduction in Plants",
+      "Forest-Our Lifeline"
+    ],
+    "Social": [
+      "Reading and Making Maps",
+      "Globe – A Model of the Earth",
+      "Land Forms",
+      "India – Our Motherland",
+      "Indian Rivers",
+      "Major Domains of the Earth",
+      "Andhra Pradesh: Land and Rivers",
+      "Andhra Pradesh: Climate and Vegetation"
+    ]
+  },
+
+  "7th Class": {
+    "Telugu": [
+      "పిల్లల పండుగ",
+      "జన్మభూమి",
+      "మా మావు",
+      "ప్రకృతి మనిషి",
+      "పల్లె ప్రస్థానం",
+      "సముద్ర గమనం",
+      "భగవంతుని దయ",
+      "స్వాతంత్ర్య వీరుడు",
+      "జ్ఞాన యోగా",
+      "మన ఊరుని పండుగలు",
+      "అన్నదాత",
+      "విజ్ఞాన శాస్త్రవేత్త",
+      "మన ఊరుని ఆటలు"
+    ],
+    "Hindi": [
+      "हम पंछी उन्मुक्त गगन के",
+      "दादी माँ",
+      "हिमालय की बेटियाँ",
+      "कठपुतली",
+      "मिठाईवाला",
+      "रक्त और हमारा शरीर",
+      "पापा खो गए",
+      "शाम - एक किसान",
+      "चिड़िया की बच्ची",
+      "अपूर्व अनुभव",
+      "रहीम के दोहे",
+      "कंचा",
+      "एक तिनका"
+    ],
+    "English 1": [
+      "Three Questions",
+      "A Gift of Chappals",
+      "The Desert",
+      "The Cop and the Anthem",
+      "Golu Grows a Nose",
+      "I Want Something in a Cage",
+      "Chandni",
+      "The Bear Story",
+      "A Tiger in the House"
+    ],
+    "English 2": [],
+    "Math 1": [
+      "Integers",
+      "Fractions and Decimals",
+      "Data Handling",
+      "Simple Equations",
+      "Lines and Angles",
+      "The Triangle and its Properties"
+    ],
+    "Math 1 IIT": [
+      "Number System",
+      "Integers",
+      "Fractions and Decimals",
+      "Divisibility Rules",
+      "Polygons",
+      "Rational Numbers",
+      "Linear Equations in One Variable",
+      "Linear Equations in Two Variables",
+      "Measures Lines and Angles",
+      "Triangles",
+      "Ratio and Proportions",
+      "Formula and Their Uses",
+      "Data Handling"
+    ],
+    "Math 2": [
+      "Congruence of Triangles",
+      "Comparing Quantities",
+      "Rational Numbers",
+      "Practical Geometry",
+      "Perimeter and Area",
+      "Algebraic Expressions",
+      "Exponents and Powers",
+      "Symmetry",
+      "Visualizing Solid Shapes"
+    ],
+    "Math 2 IIT": [
+      "Congruency of Triangles",
+      "Algebraic Expressions",
+      "Algebraic Expressions LCM & HCF",
+      "Factorisation",
+      "Area and Perimeter",
+      "Circles and Rings",
+      "Exponents and Powers",
+      "Progressions",
+      "Mensuration",
+      "Commercial Mathematics"
+    ],
+    "Math IIT": [],
+    "Physics": [
+      "Heat",
+      "Motion and Time",
+      "Electric Current and Its Effects",
+      "Winds, Storms and Cyclones",
+      "Light"
+    ],
+    "Physics IIT & NEET": [
+      "Units and Measurements",
+      "Kinematics",
+      "Newton's Laws of Motion",
+      "Work-Energy and Power",
+      "Fluid Pressure",
+      "Heat",
+      "Electricity"
+    ],
+    "Chemistry": [
+      "Acids, Bases and Salts",
+      "Physical and Chemical Changes",
+      "Water: A Precious Resource",
+      "Soil"
+    ],
+    "Chemistry IIT & NEET": [
+      "States of Matter",
+      "Basic Concepts of Chemistry",
+      "Language of Chemistry",
+      "Atomic Structure",
+      "Classification of Elements",
+      "Chemical Bonding",
+      "Solutions",
+      "The Chemistry of Laboratory"
+    ],
+    "Biology": [
+      "Nutrition in Plants",
+      "Nutrition in Animals",
+      "Respiration in Organisms",
+      "Transportation in Animals and Plants",
+      "Reproduction in Plants",
+      "Forests: Our Lifeline",
+      "Waste Disposal"
+    ],
+    "Biology NEET": [
+      "Living World",
+      "Cell - The Structural and Functional Unit of Life",
+      "Cell Cycle and Cell Division",
+      "Transport in Plants",
+      "Plant Growth and Development",
+      "Locomotion and Movement",
+      "Sexual Reproduction in Plants",
+      "Strategies in Enhancement Food Production",
+      "Microbes in Human Welfare",
+      "Human Health and Disease",
+      "Ecosystem",
+      "Bio Diversity"
+    ],
+    "Social": [
+      "Equality in Indian Democracy",
+      "State Government",
+      "Our Country – India",
+      "India’s Neighbors",
+      "The Mughal Empire",
+      "Rulers and Buildings",
+      "Mobile and Settled Communities",
+      "Popular Beliefs and Religious Debates"
+    ]
+  },
+
+  "8th Class": {
+    "Telugu": [
+      "ఆత్మీయుడు",
+      "పల్లకీ",
+      "జ్ఞానం మిన్నయిన మూర్తి",
+      "స్నేహం",
+      "మా ఊరు",
+      "కనుబడని దేవుడు",
+      "స్వాతంత్ర్యం",
+      "హరితహారం"
+    ],
+    "Hindi": [
+      "ध्वनि",
+      "लाख की चूड़ियाँ",
+      "बस की यात्रा",
+      "दीवानों की हस्ती",
+      "चिट्ठियों की अनूठी दुनिया",
+      "भगवान के डाकिए",
+      "क्या निराश हुआ जाए",
+      "यह सब से कठिन समय नहीं",
+      "कबीर की साखियाँ",
+      "कामचोर",
+      "जब सिनेमा ने बोलना सीखा",
+      "सुदामा चरित",
+      "जहाँ चाह वहाँ राह"
+    ],
+    "English 1": [
+      "My Mother",
+      "Children, Speak Up!",
+      "The Home Coming",
+      "Unity in Diversity in India",
+      "The World’s Most Dangerous Game",
+      "Oliver Asks for More",
+      "The Village Blacksmith",
+      "A Hero"
+    ],
+    "English 2": [],
+    "Math 1": [
+      "Rational Numbers",
+      "Linear Equations in One Variable",
+      "Understanding Quadrilaterals",
+      "Practical Geometry",
+      "Data Handling",
+      "Squares and Square Roots"
+    ],
+    "Math 1 IIT": [
+      "Number Theory - Basics",
+      "Number Theory - LCM, HCF & Applications",
+      "Progressions",
+      "Linear Equations One & Two Variable",
+      "Plane Geometry - Lines & Angles",
+      "Plane Geometry - Triangles",
+      "Quadratic Equations",
+      "Exponents & Powers; Square Roots & Cube Roots",
+      "Arithmetic-Direct & Inverse Proportion",
+      "Arithmetic-Profit and Loss",
+      "Algebraic Expressions - Basics"
+    ],
+    "Math 2": [
+      "Cubes and Cube Roots",
+      "Comparing Quantities",
+      "Algebraic Expressions and Identities",
+      "Visualizing Solid Shapes",
+      "Mensuration",
+      "Exponents and Powers",
+      "Direct and Inverse Proportions",
+      "Factorization",
+      "Introduction to Graphs"
+    ],
+    "Math 2 IIT": [
+      "Algebraic Expressions - Multiplications & Divisions",
+      "Area of Plane Figures",
+      "Polynomial - Factor, Remainder, Theorems",
+      "Polynomials - Factorization",
+      "Mensuration - Prism, Cube & Cuboid",
+      "Mensuration - Cylinder, Cone, Sphere",
+      "Trigonometry",
+      "Coordinate Geometry-Distance & Section Formulae",
+      "Coordinate Geometry - Area of Triangle & Quadrilateral",
+      "Sets",
+      "Logarithms"
+    ],
+    "Physics": [
+      "Force and Pressure",
+      "Friction",
+      "Sound",
+      "Chemical Effects of Electric Current",
+      "Light",
+      "Stars and the Solar System"
+    ],
+    "Physics IIT & NEET": [
+      "Units and Dimensions",
+      "Motion in One Dimension",
+      "Basics Mathematics",
+      "Vectors",
+      "Kinematics - 2D",
+      "Newton's Laws of Motion",
+      "Work-Energy and Power",
+      "Circular Motion"
+    ],
+    "Chemistry": [
+      "Synthetic Fibres and Plastics",
+      "Metals and Non-Metals",
+      "Coal and Petroleum",
+      "Combustion and Flame"
+    ],
+    "Chemistry IIT & NEET": [
+      "Basic Principles of Chemistry",
+      "Atomic Structure",
+      "Classification of Elements",
+      "Chemical Bonding",
+      "Stoichiometry-I",
+      "Gaseous State-I",
+      "General Organic Chemistry-I",
+      "Hydrogen & Its Compounds",
+      "I A Group Elements"
+    ],
+    "Biology": [
+      "Crop Production and Management",
+      "Microorganisms: Friend and Foe",
+      "Conservation of Plants and Animals",
+      "Cell: Structure and Functions",
+      "Reaching the Age of Adolescence",
+      "Reproduction in Animals"
+    ],
+    "Biology NEET": [
+      "Biological Classification",
+      "Plant Kingdom",
+      "Animal Kingdom",
+      "Morphology Of Flowering Plants",
+      "Structural Organization In Animals",
+      "Anatomy Of Flowering Plants",
+      "Mineral Nutrition",
+      "Photosynthesis In Higher Plants",
+      "Respiration In Plants",
+      "Digestion And Absorption",
+      "Breathing And Exchange Of Gases"
+    ],
+    "Social": [
+      "Resources",
+      "Land, Soil, Water, Natural Vegetation and Wildlife Resources",
+      "Mineral and Power Resources",
+      "Agriculture",
+      "Industries",
+      "Human Resources"
+    ]
+  },
+
+  "9th Class": {
+    "Telugu": [
+      "స్వాగతం",
+      "ప్రియమనసా",
+      "పద్మనాభం కథ",
+      "స్నేహ బంధం",
+      "మన తెలంగాణ",
+      "జ్ఞానం",
+      "అభియాన్"
+    ],
+    "Hindi": [
+      "दो बैलों की कथा",
+      "ल्हासा की ओर",
+      "उपभोक्तावाद की संस्कृति",
+      "साँवले सपनों की याद",
+      "मेरे बचपन के दिन",
+      "एक फूल की चाह",
+      "अन्नदाता",
+      "कौन",
+      "संकल्प"
+    ],
+    "English 1": [
+      "The Duck and the Kangaroo",
+      "The Snake and the Mirror",
+      "The Happy Prince",
+      "A Truly Beautiful Mind",
+      "The Road Not Taken",
+      "Wind",
+      "The Little Girl",
+      "Rain on the Roof",
+      "Weathering the Storm in Ersama"
+    ],
+    "English 2": [],
+    "Math 1": [
+      "Number Systems",
+      "Polynomials",
+      "Coordinate Geometry",
+      "Linear Equations in Two Variables",
+      "Introduction to Euclid’s Geometry",
+      "Lines and Angles",
+      "Triangles"
+    ],
+    "Math 1 IIT": [
+      "Theory of Numbers - Basics",
+      "Theory of Numbers - Congruence of Integers",
+      "Surds",
+      "Theory of Equations",
+      "Geometry - Triangles",
+      "Geometry - Similar Triangles",
+      "Geometry - Circles",
+      "Matrices",
+      "Determinants",
+      "Complex Numbers",
+      "Trigonometry - Ratios and Identities"
+    ],
+    "Math 2": [
+      "Quadrilaterals",
+      "Areas of Parallelograms and Triangles",
+      "Circles",
+      "Constructions",
+      "Heron’s Formula",
+      "Surface Areas and Volumes",
+      "Statistics",
+      "Probability"
+    ],
+    "Math 2 IIT": [
+      "Trigonometry - Multiples and Submultiples",
+      "Properties of Triangles - Sine, Cosine, Projection Rules",
+      "Properties of Triangles - Napier, Half Angle, Mollweide Rules",
+      "Coordinate Geometry - The Straight Lines",
+      "Coordinate Geometry - Foot and Image of Point",
+      "Permutations",
+      "Combinations",
+      "Binomial Theorem",
+      "Progressions - A.P and G.P",
+      "Progressions - H.P and A.G.P",
+      "Probability"
+    ],
+    "Physics": [
+      "Motion",
+      "Force and Laws of Motion",
+      "Gravitation",
+      "Work and Energy",
+      "Sound"
+    ],
+    "Physics IIT & NEET": [
+      "Basics",
+      "Centre of Mass",
+      "Collisions",
+      "Gravitation",
+      "Mechanical Properties of Solids",
+      "Mechanical Properties of Fluids",
+      "Viscosity",
+      "Surface Tension",
+      "Expansion of Solids",
+      "Expansion of Liquids and Gases",
+      "Calorimetry",
+      "Oscillations"
+    ],
+    "Chemistry": [
+      "Matter in Our Surroundings",
+      "Is Matter Around Us Pure?",
+      "Atoms and Molecules",
+      "Structure of the Atom"
+    ],
+    "Chemistry IIT & NEET": [
+      "Gaseous State - II",
+      "Stoichiometry - II",
+      "Chemical Equilibrium",
+      "Dilute Solutions",
+      "Ionic Equilibrium",
+      "Chemical Kinetics",
+      "General Organic Chemistry - I"
+    ],
+    "Biology": [
+      "The Fundamental Unit of Life (Cell)",
+      "Tissues",
+      "Diversity in Living Organisms",
+      "Why Do We Fall Ill?",
+      "Natural Resources",
+      "Improvement in Food Resources"
+    ],
+    "Biology NEET": [
+      "Origin & Evolution",
+      "How Do Organisms Reproduce",
+      "Control & Coordination",
+      "Life Process - Nutrition (Part 1)",
+      "Life Process - Nutrition (Part 2)",
+      "Genetics",
+      "Our Environment",
+      "Management of Natural Resources"
+    ],
+    "Social": [
+      "India: Size and Location",
+      "Physical Features of India",
+      "Drainage",
+      "Climate",
+      "Natural Vegetation and Wildlife",
+      "Population"
+    ]
+  },
+
+  "10th Class": {
+    "Telugu": [
+      "మన ఊరు – మనం",
+      "జగమెరిగిన మనిషి",
+      "జ్ఞానం",
+      "స్వతంత్రోద్యమ కథలు",
+      "నీలం – జ్ఞానం",
+      "పాట – స్వేచ్ఛ",
+      "శ్రీరాముడు",
+      "విజయగాథ"
+    ],
+    "Hindi": [
+      "सूरदास – पद",
+      "तुलसीदास – दोहे",
+      "जयशंकर प्रसाद – सूर के पद",
+      "सूर्यकांत त्रिपाठी 'निराला' – वह तोड़ती पत्थर",
+      "नागार्जुन – जीवन में संगीत",
+      "गिरिजाकुमार माथुर – तोप",
+      "रवींद्रनाथ ठाकुर – आत्मत्राण",
+      "सैयद अली जाफ़री – छोटे-से जीवन की पहचान",
+      "लीलाधर मंडलोई – पतझर में टूटी पत्तियाँ"
+    ],
+    "English 1": [
+      "A Letter to God",
+      "Nelson Mandela: Long Walk to Freedom",
+      "Two Stories About Flying",
+      "From the Diary of Anne Frank",
+      "The Hundred Dresses – I",
+      "The Hundred Dresses – II",
+      "Glimpses of India",
+      "Mijbil the Otter",
+      "Madam Rides the Bus",
+      "The Sermon at Benares",
+      "The Proposal"
+    ],
+    "English 2": [],
+    "Math 1": [
+      "Real Numbers",
+      "Polynomials",
+      "Pair of Linear Equations in Two Variables",
+      "Quadratic Equations",
+      "Arithmetic Progressions",
+      "Triangles",
+      "Coordinate Geometry"
+    ],
+    "Math 2": [
+      "Introduction to Trigonometry",
+      "Applications of Trigonometry",
+      "Circles",
+      "Constructions",
+      "Areas Related to Circles",
+      "Surface Areas and Volumes",
+      "Statistics",
+      "Probability"
+    ],
+    "Physics": [
+      "Chemical Reactions and Equations",
+      "Acids, Bases and Salts",
+      "Metals and Non-Metals",
+      "Carbon and Its Compounds",
+      "Periodic Classification of Elements",
+      "Light – Reflection and Refraction",
+      "Human Eye and Colourful World",
+      "Electricity",
+      "Magnetic Effects of Electric Current"
+    ],
+    "Chemistry": [
+      "Chemical Reactions and Equations",
+      "Acids, Bases and Salts",
+      "Metals and Non-Metals",
+      "Carbon and Its Compounds",
+      "Periodic Classification of Elements"
+    ],
+    "Biology": [
+      "Life Processes",
+      "Control and Coordination",
+      "How Do Organisms Reproduce?",
+      "Heredity and Evolution",
+      "Our Environment",
+      "Management of Natural Resources"
+    ],
+    "Social": [
+      "The Rise of Nationalism in Europe",
+      "Nationalism in India",
+      "The Making of a Global World",
+      "The Age of Industrialization",
+      "Print Culture and the Modern World",
+      "Resources and Development",
+      "Forest and Wildlife Resources",
+      "Water Resources",
+      "Agriculture",
+      "Minerals and Energy Resources",
+      "Manufacturing Industries",
+      "Lifelines of National Economy",
+      "Power Sharing",
+      "Federalism",
+      "Democracy and Diversity",
+      "Gender, Religion and Caste",
+      "Popular Struggles and Movements",
+      "Political Parties",
+      "Outcomes of Democracy",
+      "Challenges to Democracy",
+      "Development",
+      "Sectors of the Indian Economy",
+      "Money and Credit",
+      "Globalization and the Indian Economy",
+      "Consumer Rights"
+    ]
+  }
+};
